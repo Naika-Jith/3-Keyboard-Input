fc662f050f4c0d79f7cda7f2ed28698dd18ed9e9
Naika Jith <naika.jith@outlook.com>
2026-09-18 12:29:27 -0400

    All Done!!!

--- Program.cs
+++ Program.cs
@@ -17,7 +17,18 @@ namespace _3_Keyboard_Input
             Console.WriteLine($"You said: {response}"); //String interpolation
             Console.WriteLine("You said: " + response); //String concatenation
 
-            
+            int number;
+            Int32.TryParse(Console.ReadLine(), out number);
+
+            int number2;
+            if (Int32.TryParse(Console.ReadLine(), out number2))
+            {
+                // Code for sucessful conversion
+            }
+            else
+            {
+                // Error message, 'number' will be zero
+            }
 
 
         }
